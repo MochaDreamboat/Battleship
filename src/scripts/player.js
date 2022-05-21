dffc505a5e0ced556c98fc06e04c9f8a74e5e1d1
mochadreamboat <jersotmac@gmail.com>
2022-05-21 17:50:15 -0500

    Added methods to call enemy player's board functions for attacking.

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -4,15 +4,33 @@ import { gameBoard } from './gameBoard.js';
 let Player = {
     name: null,
     board: gameBoard(),
+    enemy: AI.board,
+    attempts: [],
     attack: function (letter,number) {
-        playerTwo.board.receiveAttack(letter,number);
-    }
+        if (this.attempts.find([letter, number] == true)) {
+            return;
+        }
+        this.attempts.push([letter, number]);
+        return this.enemy.receiveAttack(letter,number);
+    },
+
+    attempts: []
 }
 
 let AI = {
     name: null,
     board: gameBoard(),
+    enemy: Player.board,
+    attempts: [],
     attack: function (letter,number) {
-        playerOne.board.receiveAttack(letter,number);
+        if (this.attempts.find([letter, number] == true)) {
+            return;
+        }
+        this.attempts.push([letter, number]);
+        return Player.board.receiveAttack(letter,number);
+    },
+
+    randomizeCoordinate: function () {
+        // 
     }
 }
