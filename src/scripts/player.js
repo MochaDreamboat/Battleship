16ad762ed7b95d1632099df9759b551f86c4a47a
mochadreamboat <jersotmac@gmail.com>
2022-06-05 13:00:57 -0500

    Refactored board generation for player/AI as setter function.

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,25 +1,26 @@
 // Given player interacts with opponent's gameboard.
-import { gameBoard } from './gameBoard.js';
-
 let Player = {
     name: null,
-    board: gameBoard(),
+    setBoard: function(fn) {
+        this.board = fn();
+    },
     enemy: AI.board,
     attempts: [],
     attack: function (letter,number) {
         if (this.attempts.find([letter, number] == true)) {
-            return;
+            return false;
         }
         this.attempts.push([letter, number]);
         return this.enemy.receiveAttack(letter,number);
     },
-
     attempts: []
 }
 
 let AI = {
     name: null,
-    board: gameBoard(),
+    setBoard: function(fn) {
+        this.board = fn();
+    },
     enemy: Player.board,
     attempts: [],
     attack: function (letter,number) {
@@ -29,8 +30,4 @@ let AI = {
         this.attempts.push([letter, number]);
         return Player.board.receiveAttack(letter,number);
     },
-
-    randomizeCoordinate: function () {
-        // 
-    }
 }
